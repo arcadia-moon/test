a480e47c3507615846d2903e87e6c6c3b7d19ee9
Arcadia <arcadia@aias.io>
2023-09-26 13:05:24 +0000

    Fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/test'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: 'SCTM - %s',
