fd31d85fafb0bc431b92c88a75245d115206c931
arcadia-moon <arcadia-moon@users.noreply.github.com>
2023-09-26 13:39:09 +0000

    deploy: 63c5b65a50d5f05b805308203274c449608de1ba

--- _nuxt/static/1695735213/tactics/TA0008/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1695735213",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftactics\u002FTA0008",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1695735542/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprofile","\u002Ftechniques","\u002Ftactics","\u002Fwhy","\u002Ftactics\u002FTA0001","\u002Ftactics\u002FTA0002","\u002Ftactics\u002FTA0003","\u002Ftactics\u002FTA0004","\u002Ftactics\u002FTA0005","\u002Ftactics\u002FTA0006","\u002Ftactics\u002FTA0007","\u002Ftactics\u002FTA0008"]})
