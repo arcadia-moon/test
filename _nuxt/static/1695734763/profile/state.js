3843a78990be49027bd8ec364055569055c1dae8
arcadia-moon <arcadia-moon@users.noreply.github.com>
2023-09-26 13:27:35 +0000

    deploy: 39d95db72f4643d46156eeccfc13f8f9fd57568e

--- _nuxt/static/1695734763/profile/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fundefined\u002F_nuxt\u002Fstatic\u002F1695734763",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprofile",config:{_app:{basePath:"\u002Fundefined\u002F",assetsPath:"\u002Fundefined\u002F_nuxt\u002F",cdnURL:a}}}}(null));
