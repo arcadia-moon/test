94bb4cd4b132d7b7ca8a0e905a64df8ed9a7b8da
arcadia-moon <arcadia-moon@users.noreply.github.com>
2023-09-26 15:14:53 +0000

    deploy: 3c9bd46d2263b554c2453576787d1966889e781d

--- _nuxt/static/1695737545/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest\u002F_nuxt\u002Fstatic\u002F1695737545",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest\u002F",assetsPath:"\u002Ftest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
