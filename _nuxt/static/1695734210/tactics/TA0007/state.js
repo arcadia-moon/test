3c235c8df47fc3fa66fc3255dd7bb04b52d7a3f4
arcadia-moon <arcadia-moon@users.noreply.github.com>
2023-09-26 13:24:05 +0000

    deploy: ca0911c7a2848d4cd2f296b425a8f49a47445f0c

--- _nuxt/static/1695734210/tactics/TA0007/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fundefined\u002F_nuxt\u002Fstatic\u002F1695734210",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftactics\u002FTA0007",config:{_app:{basePath:"\u002Fundefined\u002F",assetsPath:"\u002Fundefined\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1695734639/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fundefined\u002F_nuxt\u002Fstatic\u002F1695734639",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fundefined\u002F",assetsPath:"\u002Fundefined\u002F_nuxt\u002F",cdnURL:a}}}}(null));
